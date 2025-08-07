246b3a70d1a6c98f4b000f4dae88c27fc4766e90
Saadia Bouloudene <saadiabouloudene@gmail.com>
2025-08-07 22:35:17 +0200

    chore: fix db for local tests vs for deploy to avoid errors on render

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Lancer l'application Flask en arrière-plan
+#!/bin/bash
+
+# Lancer Flask en arrière-plan
 python run.py &
 
-# Lancer l'API FastAPI en arrière-plan
-uvicorn api.main:app --reload &
+# Lancer FastAPI
+uvicorn api.main:app --host=0.0.0.0 --port=10000 &
+
+# Attendre que l’un des deux processus s'arrête
+wait -n
+
+#bloc suivant à activer pour dev local, danc e cas, il faut commenter les lignes ci-dessus
+# # Lancer l'application Flask en arrière-plan
+# python run.py &
+
+# # Lancer l'API FastAPI en arrière-plan
+# uvicorn api.main:app --reload &
