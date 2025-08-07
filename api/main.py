7865086c65946a0a20ac541738046634c0a02c50
Saadia Bouloudene <saadiabouloudene@gmail.com>
2025-08-07 22:46:44 +0200

    chore: fix db for local tests vs for deploy to avoid errors on render

--- api/main.py
+++ api/main.py
@@ -7,22 +7,19 @@ from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()  # Charge automatiquement les variables du fichier .env
 
 def create_api_app():
-        
-        #bloc 1: pour deploy on render
-        if os.getenv("ENV") != "test":
-            gcp_credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    # Bloc 1: pour déploiement sur Render
+    if os.getenv("ENV") != "test":
+        gcp_credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
         if not gcp_credentials_json:
             raise RuntimeError("La variable GOOGLE_APPLICATION_CREDENTIALS_JSON n'est pas définie")
 
-        # bloc 2: Écrit le fichier temporaire à partir du contenu JSON
+        # Écrit le fichier temporaire à partir du contenu JSON
         creds_path = "/tmp/gcp_creds.json"
         with open(creds_path, "w") as f:
             f.write(gcp_credentials_json)
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_path
 
-
-    ###bloc pour execution locales à décommenter si on lance en local et commenter le bloc ci dessus
-    # # Vérifier le fichier credentials uniquement hors test
+    # Bloc local (à activer pour exécution locale)
     # gcp_creds = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
     # if os.getenv("ENV") != "test":
     #     if not gcp_creds or not os.path.exists(gcp_creds):
@@ -44,5 +41,3 @@ def create_api_app():
 
     app.include_router(dashboard.router, prefix="/api/dashboard")
     return app
-
-#app.include_router(health.router)
