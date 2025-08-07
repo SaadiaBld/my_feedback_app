ea9627ae9d55821afba6430f915e80f07a3a53b6
Saadia Bouloudene <saadiabouloudene@gmail.com>
2025-08-07 14:07:03 +0200

    chore: tests automation

--- config.py
+++ config.py
@@ -5,15 +5,21 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    ENV = os.getenv("ENV", "dev")
 
-    # Database configuration
-    POSTGRES_USER = os.getenv('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    if ENV == "test":
+        SQLALCHEMY_DATABASE_URI = ("postgresql://postgres:postgres@localhost:5432/test_db")
+    else:
+        # Database configuration
+        POSTGRES_USER = os.getenv('POSTGRES_USER')
+        POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+        POSTGRES_DB = os.getenv('POSTGRES_DB')
+        POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+        POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+        
+        if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB]):
+            raise ValueError("Les variables d'environnement de la base de données (POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB) ne sont pas toutes définies.")
 
-    if not all([POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB]):
-        raise ValueError("Les variables d'environnement de la base de données (POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB) ne sont pas toutes définies.")
-
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{urlquote(POSTGRES_PASSWORD)}@localhost:5432/{POSTGRES_DB}"
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{urlquote(POSTGRES_PASSWORD)}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
