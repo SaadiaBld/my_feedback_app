ddac5ab71bea4639936aaea5d4dda4f6d560ba40
Saadia Bouloudene <saadiabouloudene@gmail.com>
2025-08-09 16:36:06 +0200

    chore: create render.yaml for app

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
 from passlib.hash import bcrypt
-from app import db, login_manager
-from models import User
+from .. import db, login_manager
+from ..models import User
 
 auth_bp = Blueprint("auth", __name__)
 
