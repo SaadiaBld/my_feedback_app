267eab871cf8a8cb74fe9f7de5f45dab8db4e6b6
Saadia Bouloudene <saadiabouloudene@gmail.com>
2025-08-18 16:04:49 +0200

    fix(app): auth error, migration change in alembic.ini

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'app_user'  # Flask-SQLAlchemy va gérer les guillemets
+    __tablename__ = 'app_user'  
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
